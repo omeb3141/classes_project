d55ff07906d19b02ea49de31d220b6915fa1e00f
97252 <omer.bahir10@gmail.com>
2025-06-26 11:00:52 +0300

    changed mail update test

--- test2.py
+++ test2.py
@@ -2,9 +2,6 @@ from classes_project_code import MahaElintStudent, MahaStudent
 
 def test_fail_id():
     m1 = MahaStudent("Omer Bahir", 54, "omerbahir10@gmail.com", "2025-03-19", "Elint")
-def test_fail_update_mail():
-    m1 = MahaStudent("Omer Bahir", 543700421, "omerbahir10@gmail.com", "2025-03-19", "Elint")
-    m1.Mail = "omerbahir10@gmail.c.om"
 
 def test_fail_recruit_date():
     m1 = MahaStudent("Omer Bahir", 543700421, "omerbahir10@gmail.com", "2025-08-19", "Elint")
